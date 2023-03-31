de1ab78b2708291174de514254a42da70d00e4b6
varm <v.racaud-minuzzi@orange.fr>
2023-03-31 12:00:06 +0200

    fix: return in the function on sending response

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -2,23 +2,23 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req, res) => {
-  res.status(200).send("post1, post2, post3")
+  return res.status(200).send("post1, post2, post3")
 })
 
 router.get("/:id", (req, res) => {
   const { id } = req.params
   if (!id || isNaN(id)) {
-    res.status(400).send("Please provide a valid id")
+    return res.status(400).send("Please provide a valid id")
   }
-  res.status(200).send(`Post with id: ${id}`)
+  return res.status(200).send(`Post with id: ${id}`)
 })
 
 router.post("/", (req, res) => {
   const { title, content } = req.body
   if (!title || !content) {
-    res.status(400).send("Please provide both title and content")
+    return res.status(400).send("Please provide both title and content")
   }
-  res
+  return res
     .status(201)
     .send(`Post created with title: ${title} and content: ${content}`)
 })
@@ -27,17 +27,19 @@ router.put("/content/:id", (req, res) => {
   const { id } = req.params
   const { content } = req.body
   if (!id || isNaN(id) || !content) {
-    res.status(400).send("Please provide a valid id and content")
+    return res.status(400).send("Please provide a valid id and content")
   }
-  res.status(200).send(`Post updated with id: ${id} with content: ${content}`)
+  return res
+    .status(200)
+    .send(`Post updated with id: ${id} with content: ${content}`)
 })
 
 router.delete("/:id", (req, res) => {
   const { id } = req.params
   if (!id || isNaN(id)) {
-    res.status(400).send("Please provide a valid id")
+    return res.status(400).send("Please provide a valid id")
   }
-  res.status(200).send(`Post deleted with id: ${id}`)
+  return res.status(200).send(`Post deleted with id: ${id}`)
 })
 
 module.exports = router
