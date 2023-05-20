346469dee708d1019044424b1b8f691448f1eac7
vavarm <v.racaud-minuzzi@orange.fr>
2023-05-20 17:20:32 +0200

    fix: print the database url on start

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,14 @@
 const { Sequelize } = require("sequelize")
 
+console.log("database.js: " + process.env.DATABASE_URL)
+
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
+  process.env.DATABASE_URL,
   {
-    host: process.env.DB_HOSTNAME,
-    dialect: "postgres",
-    port: process.env.DB_PORT,
     logging: false,
   }
 )
+
+
 
 module.exports = sequelize
