c0aef468190c1096ef0d46b83fac5528adc13b68
vavarm <v.racaud-minuzzi@orange.fr>
2023-05-29 10:37:47 +0200

    debug: print auth credentials on authentication

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,9 @@
 const jwt = require("jsonwebtoken")
 
 module.exports = (req, res, next) => {
+  console.log("auth.js: JWT_SECRET: " + process.env.JWT_SECRET)
+  console.log("auth.js: cookie: " + req.cookies)
+  console.log("auth.js: token: " + req.cookies.token)
   try {
     const token = req.cookies.token
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
