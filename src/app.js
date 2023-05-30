eda899776ccbb5624e5e335f370c1fa77342ce46
vavarm <v.racaud-minuzzi@orange.fr>
2023-05-30 06:11:14 +0200

    fix: change header options

--- src/app.js
+++ src/app.js
@@ -18,10 +18,19 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL)
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
-  next();
+  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    // Respond to preflight requests
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 })
+
 // public folder
 app.use(express.static(__dirname + '/../public'))
 // Routes
