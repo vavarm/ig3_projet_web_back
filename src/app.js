6c491fa06db2694c47f2aa335914c77364c75ede
vavarm <v.racaud-minuzzi@orange.fr>
2023-06-13 21:16:10 +0200

    fix: change X-Frame-Options

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.setHeader('Access-Control-Allow-Credentials', 'true')
-  res.setHeader('X-Frame-Options', process.env.CLIENT_URL+'/')
+  res.setHeader('X-Frame-Options', `ALLOW-FROM ${process.env.CLIENT_URL}`)
 
   if (req.method === 'OPTIONS') {
     // Respond to preflight requests
