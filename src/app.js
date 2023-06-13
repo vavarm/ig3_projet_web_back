980da4f065d68f2fd01bd9b041b08236860a3702
vavarm <v.racaud-minuzzi@orange.fr>
2023-06-13 21:28:32 +0200

    fix: change Content Security Policy

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.setHeader('Access-Control-Allow-Credentials', 'true')
-  res.setHeader('X-Frame-Options', `ALLOW-FROM ${process.env.CLIENT_URL}`)
+  res.setHeader('Content-Security-Policy', `default-src 'self' ${process.env.CLIENT_URL}`)
 
   if (req.method === 'OPTIONS') {
     // Respond to preflight requests
